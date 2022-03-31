4d0ded3cee4db6246b2d4114acae69ed2f43477b
Jeremy Mann <jmann277@gmail.com>
2022-03-31 12:47:18 -0700

    make URL PascalCase

--- src/components/about.js
+++ src/components/about.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 const Anchor = styled.a`
   color: #000000;
 `;
-const URL = ({ url, description }) => {
+const Url = ({ url, description }) => {
   return (
     <Anchor href={url} target="new">
       {description}
@@ -30,12 +30,12 @@ const AboutMe = () => {
   return (
     <div>
       <Paragraph>
-        I'm a <URL url={linkedin} description={linkedin_description} />{" "}
+        I'm a <Url url={linkedin} description={linkedin_description} />{" "}
         specializing in healthcare. Previously, I was a mathematician and a
-        teacher. In my free time, I enjoy creating{" "}
-        <URL url={github} description={github_description} /> and thinking about
+        teacher. In my free time, I enjoy 
+        <Url url={github} description={github_description} /> and thinking about
         longevity. Sometimes, I{" "}
-        <URL url={youtube} description={youtube_description} />.
+        <Url url={youtube} description={youtube_description} />.
       </Paragraph>
     </div>
   );
