0447c15f5ce94127f18c7ed7a0066fd3c94bd919
Jeremy Mann <jmann277@gmail.com>
2022-04-01 18:59:38 -0700

    add pde page

---
+++ src/pages/pde.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+import { Link } from 'gatsby'
+
+const Pde = () => {
+    const title = 'PDE = Personal Development Environment'
+    return (
+        <Layout title={title}>
+            Talk about your PDE.
+        </Layout >
+    )
+}
+
+export default Pde
