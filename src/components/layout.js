d28724e24cf7cd8964844876122b19978369d27b
Jeremy Mann <jmann277@gmail.com>
2022-04-03 11:48:20 -0700

    use layout in index

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,15 +3,13 @@ import styled from "styled-components";
 import { Helmet } from "react-helmet";
 import NavWindow from "../components/links_at_the_top";
 
-const Page = styled.html`
-   background: #eefaff;
-`
 const Body = styled.body`
+  margin: 0px;
   font-family: "optima";
+  /* background: #eefaff; */
   text-align: center;
   display: grid;
   padding-top: 5vh;
-  vertical-align: middle;
   justify-content: center;
   @media (max-width: 600px) {
     padding: 5vh;
@@ -27,19 +25,18 @@ const Container = styled.div`
 `;
 const Layout = ({ title, children }) => {
   return (
-    <Page>
+    <html lang="en">
       <Helmet>
-        <title>Jeremy P. Mann</title>
+        <title>{title}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Helmet>
       <Body>
         <NavWindow />
         <Container>
-          <h1>{title}</h1>
           {children}
         </Container>
       </Body>
-    </Page>
+    </html>
   );
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,44 +5,19 @@ import AboutMe from "../components/about";
 import HeadshotImage from "../components/headshot";
 import NameTitle from "../components/name_title";
 import NavWindow from "../components/links_at_the_top";
+import Layout from "../components/layout";
 import { Helmet } from "react-helmet";
 
-const Body = styled.body`
-  margin: 0;
-  font-family: "optima";
-  background: #eefaff;
-  text-align: center;
-  display: flex;
-  padding-top: 5vh;
-  justify-content: center;
-  @media (max-width: 600px) {
-    padding: 5vh;
-  }
-`;
-const Container = styled.div`
-  border: 1px solid #000000;
-  padding: 4rem;
-  max-width: 600px;
-  @media (max-width: 600px) {
-    height: 75vh;
-    padding: 2rem;
-`;
 const IndexPage = () => {
-  return (
-    <html lang="en">
-      <Helmet>
-        <title>Jeremy P. Mann</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Helmet>
-      <Body>
-        <Container>
-          <NameTitle />
-          <HeadshotImage />
-          <AboutMe />
-        </Container>
-      </Body>
-    </html>
+  const content = (
+    <div>
+      <NameTitle />
+      <HeadshotImage />
+      <AboutMe />
+    </div>
   );
+  const title = "Jeremy P. Mann";
+  return <Layout title={title}>{content}</Layout>;
 };
 
 export default IndexPage;
