d04b1ee0991e6507321be54adc3788fa9a5de942
Jeremy Mann <jmann277@gmail.com>
2022-04-07 10:32:15 -0700

    add pde description and text

--- src/pages/pde.js
+++ src/pages/pde.js
@@ -1,13 +1,18 @@
 import * as React from "react";
 import Layout from "../components/layout";
 import Paragraph from "../components/paragraph";
+import Url from "../components/url";
 
 const Pde = () => {
   const title = "PDE";
+  const pde_description = "github";
+  const pde_url = "https://github.com/jmann277/dotfiles";
   const content = (
         <Paragraph>
-            Talk about your dotfiles. Make sure to say more than you need to
-            in order for it to fill up all the space.
+            PDE is an acronym for personal development environment.
+            I primarily use a combination of tmux and neovim.
+            You can find the source code for my PDE
+            on <Url url={pde_url} description={pde_description} />
         </Paragraph>
     );
   return <Layout title={title}>{content}</Layout>;
