46e1c25009dba6678dde88c86f6149e0b4151aa4
Jeremy Mann <jmann277@gmail.com>
2022-04-07 10:31:56 -0700

    reduct margin in headshot

--- src/components/headshot.js
+++ src/components/headshot.js
@@ -4,10 +4,10 @@ import styled from "styled-components";
 
 const Image = styled.img`
   height: 400px;
-  margin: 2rem 0;
+  margin: 1rem 0;
   @media (max-width: 600px) {
     height: 200px;
-    margin: 2rem 0;
+    margin: 1rem 0;
   }
 `;
 const HeadshotImage = () => {
