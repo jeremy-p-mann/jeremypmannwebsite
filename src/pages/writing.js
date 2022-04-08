4cf2ee1898838149d5b3bd4d7e113c537d028a2d
Jeremy Mann <jmann277@gmail.com>
2022-04-07 20:03:30 -0700

    add links for writing

--- src/pages/writing.js
+++ src/pages/writing.js
@@ -1,38 +1,49 @@
 import * as React from "react";
 import Layout from "../components/layout";
 import Paragraph from "../components/paragraph";
+import Url from "../components/url";
+import graph_qm from "../pdfs/graph_qm.pdf";
+import cs_theory from "../pdfs/cs_notes.pdf";
+import alexander_duality from "../pdfs/Alexander_Duality_in_Image_Analysis.pdf";
 
 const Writing = () => {
   const title = "Writing";
-  const alexander_duality = 0;
-  const graph_qm = 0;
-  const cs_theory = 0;
-  const picture_maths = 0;
-  const undergrad_thesis = 0;
+  const ad_description = "Alexander Duality in Image Analysis";
+  const graph_qm_description =
+    "(Graph) Quantum Mechanics: Towards a Homotopical Approach to Spectral Graph Theory.";
+  const cs_theory_description =
+    "Notes on Abelian Chern-Simons Theory (For Homotopy Theorists) ";
+  const pm_descr = "Defining topology through interviews: Jeremy Mann";
+  const pm_url =
+    "https://picturethismaths.wordpress.com/2016/10/28/defining-topology-through-interviews-interview-seven-with-jeremy-mann/";
+  const thesis_url =
+    "https://scholar.rose-hulman.edu/cgi/viewcontent.cgi?article=1074&context=rhumj";
+  const thesis_description =
+    "Equilateral Dimension of Riemannian Manifolds with Bounded Curvature.";
   const content = (
     <main>
       <Paragraph>
-        I wrote a “not-suitable-for-publication” outline on using Alexander
-        Duality in Image Analysis.
+        I wrote a “not-suitable-for-publication” outline on using{" "}
+        <Url url={alexander_duality} description={ad_description} />.
       </Paragraph>
       <Paragraph>
         I wrote some rough notes going through the BV-approach to scalar field
-        theory on a graph: (Graph) Quantum Mechanics: Towards a Homotopical
-        Approach to Spectral Graph Theory.
+        theory on a graph:{" "}
+        <Url url={graph_qm} description={graph_qm_description} />.
       </Paragraph>
       <Paragraph>
-        I wrote some Notes on Abelian Chern-Simons Theory (For Homotopy
-        Theorists) based on a lecture given at the Notre Dame Graduate Student
-        Topology Seminar.
+        I wrote some <Url url={cs_theory} description={cs_theory_description} />
+        based on a lecture given at the Notre Dame Graduate Student Topology
+        Seminar.
       </Paragraph>
       <Paragraph>
         I was interviewed by Picture this Maths in celebration of the Nobel
-        Prize in Physics for topological phases of matter: Defining topology
-        through interviews: Jeremy Mann.
+        Prize in Physics for topological phases of matter.{" "}
+        <Url url={pm_url} description={pm_descr} />.
       </Paragraph>
       <Paragraph>
         I wrote an undergraduate thesis on some stuff in Riemannian geometry:
-        Equilateral Dimension of Riemannian Manifolds with Bounded Curvature.
+        <Url url={thesis_url} description={thesis_description} />.
       </Paragraph>
     </main>
   );
