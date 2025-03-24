149a8d18d9c67b8a140963c71f2aa9271ced081d
Jeremy Mann <jmann227@gmail.com>
2025-03-23 21:45:45 -0700

    change about me to sayi like coding

--- src/components/about.js
+++ src/components/about.js
@@ -7,7 +7,7 @@ const AboutMe = () => {
   const linkedin = "https://www.linkedin.com/in/jeremypmann/";
   const linkedin_description = "data scientist";
   const github = "https://www.github.com/jeremy-p-mann";
-  const github_description = "creating open source software";
+  const github_description = "coding";
   const youtube = "https://www.youtube.com/user/";
   const youtube_description = "make videos";
   return (
